db4cd69e6ab64e24ad39d1a0a8d216caeca8e73d
nmarks99 <nmarks@anl.gov>
2024-07-08 16:46:05 -0500

    convert pitch to degrees

--- iocBoot/ioc4idVDCM/cmds/AcsMotion.cmd
+++ iocBoot/ioc4idVDCM/cmds/AcsMotion.cmd
@@ -12,22 +12,22 @@ dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(PREFIX),R=asyn_1,PORT=ACS1_ETH,ADD
 # TODO: Get rid of this once we have autosave
 
 # Set ERES to EFAC for each axis
-doAfterIocInit("dbpf('$(PREFIX)m1.ERES','-5.72957795e-07')")
+doAfterIocInit("dbpf('$(PREFIX)m1.ERES','-5.72957795e-07')") # converted to deg
 doAfterIocInit("dbpf('$(PREFIX)m2.ERES','0.00005')")
 doAfterIocInit("dbpf('$(PREFIX)m3.ERES','0.00005')")
-doAfterIocInit("dbpf('$(PREFIX)m4.ERES','0.000001')")
+doAfterIocInit("dbpf('$(PREFIX)m4.ERES','2.245132426e-07')") # converted to deg
 doAfterIocInit("dbpf('$(PREFIX)m5.ERES','0.00000002677')")
 
-# set offset for bragg angle conversion
+# set offset for bragg angle to match dial indicator
 doAfterIocInit("dbpf('$(PREFIX)m1.OFF','101.26886969629788')")
 doAfterIocInit("dbpf('$(PREFIX)m1.FOFF','1')") # frozen
 
 # Set VMAX
-doAfterIocInit("dbpf('$(PREFIX)m1.VMAX','0.7')")
-doAfterIocInit("dbpf('$(PREFIX)m2.VMAX','1.0')")
-doAfterIocInit("dbpf('$(PREFIX)m3.VMAX','0.6')")
-doAfterIocInit("dbpf('$(PREFIX)m4.VMAX','0.05')")
-doAfterIocInit("dbpf('$(PREFIX)m5.VMAX','0.0004')")
+doAfterIocInit("dbpf('$(PREFIX)m1.VMAX','0.4')") # deg/s
+doAfterIocInit("dbpf('$(PREFIX)m2.VMAX','1.0')") # mm/s
+doAfterIocInit("dbpf('$(PREFIX)m3.VMAX','0.6')") # mm/s
+doAfterIocInit("dbpf('$(PREFIX)m4.VMAX','0.01')") # deg/s
+doAfterIocInit("dbpf('$(PREFIX)m5.VMAX','0.0004')") # rad/s
 
 # Set use encoder = YES
 doAfterIocInit("dbpf('$(PREFIX)m1.UEIP','1')")
